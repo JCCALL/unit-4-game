aba77d3b46f3e4e1f144c4925f922ebfe1629901
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-03-17 12:55:34 -0600

    changed file name, started javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    var charcters = {
+
+        'Batman' : {
+            hp: 150,
+            ap: 17,
+            cap: 20,
+            image: "assets/images/batman.png"
+        },
+
+        'Nightwing' : {
+            hp: 120,
+            ap: 11,
+            cap: 15,
+            image: "assets/images/nightwing.png"
+        },
+
+        'Joker' : {
+            hp: 140,
+            ap: 10,
+            cap: 19,
+            image: "assets/images/joker.png"
+        },
+
+        'Deathstroke' : {
+            hp: 160,
+            ap: 14,
+            cap: 23,
+            image: "assets/images/deathstroke.png"
+        },
+
+        'Bane' : {
+            hp: 180,
+            ap: 15,
+            cap: 25,
+            image: "assets/images/batman.png"
+        },
+    };
+
+
+
+
+});
