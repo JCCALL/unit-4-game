5dfa5a9ac46069f3f3f0b790f6beb82529f5d668
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-03-19 13:32:08 -0600

    fixing script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
 
-    var charcters = {
+    var characters = {
 
         'Batman' : {
+            name: "Batman",
             hp: 150,
             ap: 17,
             cap: 20,
@@ -10,6 +11,7 @@ $(document).ready(function () {
         },
 
         'Nightwing' : {
+            name: "Nightwing",
             hp: 120,
             ap: 11,
             cap: 15,
@@ -17,6 +19,7 @@ $(document).ready(function () {
         },
 
         'Joker' : {
+            name: "Joker",
             hp: 140,
             ap: 10,
             cap: 19,
@@ -24,6 +27,7 @@ $(document).ready(function () {
         },
 
         'Deathstroke' : {
+            name: "Deathstroke",
             hp: 160,
             ap: 14,
             cap: 23,
@@ -31,14 +35,23 @@ $(document).ready(function () {
         },
 
         'Bane' : {
+            name: "Bane",
             hp: 180,
             ap: 15,
             cap: 25,
             image: "assets/images/batman.png"
         },
     };
+    console.log(characters);
+    
+    function render(char, area){
+        var charDiv = $("<div class='character' data-name'>" + char.name +"'>");
+        var charName = $("<div class= 'character-name'>").text(char.name);
+        var charImage = $("<img alt ='image' class 'character-image'>").attr("src", char.imageUrl);
+        var charHp = $("<div classes='character-hp'>").text(char.hp);
+        charDiv.append(charName).append(charImage).append (charHp);
+        $(area).append(charDiv);
+    };
+    
+    });
 
-
-
-
-});
